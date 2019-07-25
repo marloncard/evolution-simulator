4e55a2b65e6a19eb160e0abfb5ac73d3b287ac0c
Marlon Card <marloncard@gmail.com>
2019-07-25 05:51:42 -0400

    Add animations

--- js/game.js
+++ js/game.js
@@ -11,6 +11,13 @@ const config = {
     width: 800,
     height: 600,
     backgroundColor: '#ababab',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: 100,
+            debug: true
+        }
+    },
 
     scene: {
         preload,
@@ -62,12 +69,17 @@ const anims = {
 };
 let scene = null;
 
+
+let slime = "";
+let timer = 0;
+
 function preload () {
     // preload images, sounds & other assets
     //this.load.json('map', 'assets/grass.json')
     this.load.image('tileset', 'assets/TilesetGrass/overworld_tileset_grass.png');
     this.load.tilemapTiledJSON('map', 'assets/TilesetGrass/evo-tileset.json');
-    this.load.spritesheet('tiles', 'assets/grass.png', {frameWidth: 64, frameHeight: 64})
+    //this.load.spritesheet('tiles', 'assets/grass.png', {frameWidth: 64, frameHeight: 64})
+    this.load.multiatlas('slime', 'assets/slime.json', 'assets')
     this.load.spritesheet('slime_back', 'assets/slime1_back.png', {frameWidth: 16, frameHeight: 16});
     this.load.spritesheet('slime_front', 'assets/slime1_front.png', {frameWidth: 16, frameHeight: 16});
     this.load.spritesheet('slime_side', 'assets/slime1_side.png', {frameWidth: 16, frameHeight: 16})
@@ -82,12 +94,141 @@ function create () {
     const waterLayer = map.createStaticLayer("Water", tileset, 0, 0);
     const structureLayer = map.createStaticLayer("Structures", tileset, 0, 0);
 
+    // Add organism to scene (full spritesheet)
+    this.slime = this.physics.add.sprite(400, 300,'slime', 'slime-05.png');
 
-    gameState.slime_front = this.add.sprite(400, 300, 'slime_front');
+    // Create animations
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNames('slime', {prefix: 'slime-0', start: 5, end: 8, suffix: '.png'})
+    })
+    this.anims.create({
+        key: 'north',
+        frames: this.anims.generateFrameNames('slime', {prefix:'slime-0', start: 1, end: 4, suffix: '.png'}),
+        frameRate:15,
+        repeat: -1
+    })
+    this.anims.create({
+        key: 'south',
+        frames: this.anims.generateFrameNames('slime', {prefix:'slime-0', start: 5, end: 8, suffix: '.png'}),
+        frameRate:15,
+        repeat: -1
+    })
+    this.anims.create({
+        key: 'west',
+        frames: this.anims.generateFrameNames('slime', {prefix:'slime-', start: 09, end: 12, suffix: '.png'}),
+        frameRate:15,
+        repeat: -1
+    })
+    this.anims.create({
+        key: 'east',
+        frames: this.anims.generateFrameNames('slime', {prefix:'slime-', start: -09, end: -12, suffix: '.png'}),
+        frameRate:15,
+        repeat: -1
+    })
 
 
+
+    //Group of organisms
+    this.organisms = this.physics.add.group({
+        key: 'slime',
+        repeat: 4,
+        setXY: {
+            x: 300,
+            y: 300,
+            stepX: 80,
+            stepY: 20
+        }
+    });
+
+    //scale organism
+    // Phaser.Actions.ScaleXY(this.organisms.getChildren(), -0.10,-0.10);
+
+    // Set speed
+    Phaser.Actions.Call(this.organisms.getChildren(), function(organism) {
+        organism.speed = Math.random() * 2 + 1;
+        // make item interactive
+        organism.setInteractive();
+    }, this);
+
+    locations = this.add.text(16, 16, 'location: 0, 0', { fontSize: '10px', fill: '#000' })
+    this.orgLocations = this.add.group({
+        //
+    })
+
+    this.slime.speed = 2;
+    this.physics.world.bounds.width = map.widthInPixels-10;
+    this.physics.world.bounds.height = map.heightInPixels-10;
+    this.slime.setCollideWorldBounds(true);
+    
+    
+    
 };
 function update () {
-    //
-};
 
+        // Organism movement
+        let organisms = this.organisms.getChildren();
+        let numOrganisms = organisms.length;
+    
+        for (let i = 0; i < numOrganisms; i++) {
+
+            // movement
+            movementAnim(organisms[i]);
+  
+            if (timer < 10) {
+                if (i === 0 || i === 1) {
+                    organisms[i].setVelocityY(64);  
+                } else if (i === 2 || i === 3) {
+                    organisms[i].setVelocityX(64)
+                } else if (i === 5) {
+                    organisms[i].setVelocity(0,0)
+                }
+            }
+
+            
+
+
+                if (organisms[i].y >= 599) {
+                    organisms[i].setVelocityY(-64)
+                    //organisms[i].anims.play('north', true);
+                } else if (organisms[i].y <= 1) {
+                    organisms[i].setVelocityY(64);
+                    //organisms[i].anims.play('south', true);
+                } else if (organisms[i].x >= 799) {
+                    organisms[i].setVelocityX(-64);
+                    //organisms[i].anims.play('west', true)
+                } else if (organisms[i].x <= 1) {
+                    organisms[i].setVelocityX(64);
+                    //organisms[i].anims.play('west', true)
+                    //organisms[i].flipX = true;
+                }
+                
+    // if (organisms[i].x >= this.organismMaxX && organisms[i].speed > 0) {
+            //     organisms[i].speed *= -0.25;
+            // } else if (organisms[i].x <= this.organismMinX && organisms[i].speed < 0) {
+            //     organisms[i].speed *= -0.25;
+            // }
+            timer++
+        };
+
+    //this.slime.y -= this.slime.speed
+    //this.slime.anims.play('north', true);
+
+}; 
+
+function movementAnim(obj) {
+    if(obj.body.velocity.y > 0) {
+        obj.anims.play('south', true);
+    } else if (obj.body.velocity.y < 0) {
+        obj.anims.play('north', true);
+    } else if (obj.body.velocity.x < 0) {
+        obj.anims.play('west', true);
+        obj.flipX = false;
+    } else if (obj.body.velocity.x > 0) {
+        obj.anims.play('west', true)
+        obj.flipX = true;
+    } else {
+        obj.anims.play('idle', true)
+    }
+
+}
