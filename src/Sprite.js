4b6be47b9ce8c547675532aee086236a23764f07
Marlon Card <marloncard@gmail.com>
2020-03-24 09:39:20 -0400

    Update Sprite.js

--- src/Sprite.js
+++ src/Sprite.js
@@ -21,56 +21,56 @@ export class Sprite extends Phaser.Physics.Arcade.Sprite {
         this.age = 0;
         this.name = ""
 
-    }
+    };
 
-senescense(time) {
-    // Organism aging; modifies life
-    if (time % 30 === 0 && this.timedAgeArray.includes(time) === false) {
-        this.timedAgeArray.push(time)
-        this.age += 1
-        //console.log(this.name + " is now age: " + this.age)
-        this.hp -= this.age
-        //this.maxHP -=5
+    senescense(time) {
+        // Organism aging; modifies life
+        if (time % 30 === 0 && this.timedAgeArray.includes(time) === false) {
+            this.timedAgeArray.push(time)
+            this.age += 1
+            //console.log(this.name + " is now age: " + this.age)
+            this.hp -= this.age
+            //this.maxHP -=5
 
-        // Lose 5 max health per "year" after age 5
-        if (this.age > 5) {
-            this.maxHP -= 10
+            // Lose 5 max health per "year" after age 5
+            if (this.age > 5) {
+                this.maxHP -= 10
+            }
         }
-    }
-}
+    };
 
-reproduce(nameCounter, key ) {
-    // Modifies life; creates new instance of organism
-    if (this.age >= 2 && this.hp > 100) {
-        //let offspring = Object.assign(Object.create(Object.getPrototypeOf(this)), this)
-        let offspring = organisms.create(this.x, this.y, key)
-        this.hp = this.hp / 2;
-        offspring.hp = this.hp / 2;
-        offspring.name = "Org" + nameCounter;
+    reproduce(nameCounter, key ) {
+        // Modifies life; creates new instance of organism
+        if (this.age >= 2 && this.hp > 100) {
+            //let offspring = Object.assign(Object.create(Object.getPrototypeOf(this)), this)
+            let offspring = organisms.create(this.x, this.y, key)
+            this.hp = this.hp / 2;
+            offspring.hp = this.hp / 2;
+            offspring.name = "Org" + nameCounter;
+            
+            return offspring;
+        } else {
+            return null;
+        }
+
         
-        return offspring;
-    } else {
-        return null;
-    }
+    };
 
-    
-}
+    consume(food) {
+        // Restores health; chance increased by speed
+        // 80% base chance, each point of speed + 2%; 20% max
+    };
 
-consume(food) {
-    // Restores health; chance increased by speed
-    // 80% base chance, each point of speed + 2%; 20% max
-}
+    metabolise(rate, time) {
+        // Daily process which lowers health
+        // Increased by speed
 
-metabolise(rate, time) {
-    // Daily process which lowers health
-    // Increased by speed
-
-    if (time % 2 === 0 && this.timeArray.includes(time) === false) {
-        this.timeArray.push(time);
-        this.hp = this.hp - rate
-        //console.log(this.hp + " HP Remaining for: " + this.name)
-    }
-    
-}
+        if (time % 2 === 0 && this.timeArray.includes(time) === false) {
+            this.timeArray.push(time);
+            this.hp = this.hp - rate
+            //console.log(this.hp + " HP Remaining for: " + this.name)
+        }
+        
+    };
 
 }
